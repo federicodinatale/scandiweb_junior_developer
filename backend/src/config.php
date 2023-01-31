168199d8c8f93b6130e6aaa042c63ceaab1c95cf
Federico Di Natale <99074961+federicodinatale@users.noreply.github.com>
2023-01-31 08:30:33 +0100

    Update config.php

--- backend/src/config.php
+++ backend/src/config.php
@@ -3,9 +3,9 @@
 // router
 define("API_PREFIX", "/api");
 // database
-define("DB_HOST", "localhost");
-define("DB_PORT", 3306);
-define("DB_USERNAME", "fede");
-define("DB_PASSWORD", "");
-define("DB_DATABASE", "products");
+define("DB_HOST", "********");
+define("DB_PORT", ****);
+define("DB_USERNAME", "****");
+define("DB_PASSWORD", "****");
+define("DB_DATABASE", "********");
 define("DB_ERROR", true);
